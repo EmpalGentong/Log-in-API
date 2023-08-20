9f3c55de84be84e14adf2b3ac63eca6427101ef0
Empalgentong <saihan507@gmail.com>
2023-08-20 11:28:03 +0700

    connect to Database

--- server.js
+++ server.js
@@ -1,12 +1,38 @@
 const express = require("express");
-const app = express();
 require("dotenv").config();
+const sql = require("pg");
+
+const app = express();
 const dotenv = process.env;
 
-app.get("/", (req, res) => {
-  res.status(200).send("Bismillah");
+//DB init
+const client = new sql.Client({
+  user: dotenv.DB_USER,
+  host: dotenv.DB_HOST,
+  database: dotenv.DB_NAME,
+  password: dotenv.DB_PASS,
+  port: dotenv.DB_PORT,
+});
+
+app.get("/", async (req, res) => {
+  try {
+    const respond = await client.query("select * from product;");
+    res.status(200).json({ message: respond.rows });
+  } catch (err) {
+    res.status(500).json({ message: err });
+  }
 });
 
 app.listen(dotenv.PORT, () => {
-  console.log(`app listening port ${dotenv.PORT}`);
+  try {
+    client.connect((err) => {
+      if (!err) {
+        console.log(`connected to sql, app listening port ${dotenv.PORT}`);
+      } else {
+        console.log(err);
+      }
+    });
+  } catch (err) {
+    console.log(err);
+  }
 });
