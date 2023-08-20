78d5513c4946bbbb5700a9c20bf1b4aff2499fa4
Empalgentong <saihan507@gmail.com>
2023-08-20 15:49:00 +0700

    change to sequelize instead of PG dependencies

--- server.js
+++ server.js
@@ -1,38 +1,34 @@
 const express = require("express");
 require("dotenv").config();
-const sql = require("pg");
-
 const app = express();
 const dotenv = process.env;
+const { sequelize } = require("./sequelize/models");
 
-//DB init
-const client = new sql.Client({
-  user: dotenv.DB_USER,
-  host: dotenv.DB_HOST,
-  database: dotenv.DB_NAME,
-  password: dotenv.DB_PASS,
-  port: dotenv.DB_PORT,
-});
-
-app.get("/", async (req, res) => {
+const connectDB = async () => {
+  console.log("checking connection to database");
   try {
-    const respond = await client.query("select * from product;");
-    res.status(200).json({ message: respond.rows });
+    await sequelize.authenticate();
+    console.log("connected to DB");
+  } catch (err) {
+    res.json({ message: err });
+  }
+};
+
+app.get("/", (req, res) => {
+  try {
+    res.status(200).json({ message: "Bismillah" });
   } catch (err) {
     res.status(500).json({ message: err });
   }
 });
 
-app.listen(dotenv.PORT, () => {
-  try {
-    client.connect((err) => {
-      if (!err) {
-        console.log(`connected to sql, app listening port ${dotenv.PORT}`);
-      } else {
-        console.log(err);
-      }
-    });
-  } catch (err) {
-    console.log(err);
-  }
-});
+(async () => {
+  await connectDB();
+  app.listen(dotenv.PORT, () => {
+    try {
+      console.log(`app running on port ${dotenv.PORT}`);
+    } catch (err) {
+      console.log(err);
+    }
+  });
+})();
