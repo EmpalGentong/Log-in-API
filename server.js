96ed76a9afedab24904cae8e78f37307e8502be2
Empalgentong <saihan507@gmail.com>
2023-09-08 14:22:01 +0700

    db connected init

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
 const Express = require("express");
 require("dotenv").config();
+
+const { sequelize } = require("./models/");
 const app = Express();
+
+const connectDB = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("DB connected");
+  } catch (e) {
+    console.log(e);
+  }
+};
 
 app.get("/", (req, res) => {
   res.send("Running");
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`server is running on localhost:${process.env.PORT}`);
-});
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`server is running on localhost:${process.env.PORT}`);
+    });
+  })
+  .catch((e) => {
+    console.log(e);
+  });
